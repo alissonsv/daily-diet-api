7adcd331e6cf42e3a676534270f097d3024f60fe
Alisson Vargas <contato@alissonsv.com>
2024-06-05 17:11:50 -0300

    refactor: alter rollback on test to be run afterEach test

--- tests/repositories/user-repository.spec.ts
+++ tests/repositories/user-repository.spec.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from 'node:crypto';
 import { execSync } from 'node:child_process';
 
-import { describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect, beforeEach, afterEach } from 'vitest';
 import { faker } from '@faker-js/faker';
 
 import { User } from '../../src/models/User';
@@ -10,8 +10,11 @@ import { knex } from '../../src/database';
 
 describe('UserRepository', () => {
   beforeEach(() => {
-    execSync('npm run knex migrate:rollback --all');
     execSync('npm run knex migrate:latest');
+  });
+
+  afterEach(() => {
+    execSync('npm run knex migrate:rollback --all');
   });
 
   it('Should create an user on database successfully', async () => {
