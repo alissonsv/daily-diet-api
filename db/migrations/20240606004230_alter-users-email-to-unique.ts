17bfaed864587d1f531a46334d83e4936c62c5db
Alisson Vargas <contato@alissonsv.com>
2024-06-05 21:49:50 -0300

    chore: alter users email to be unique on db

---
+++ db/migrations/20240606004230_alter-users-email-to-unique.ts
@@ -0,0 +1,13 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.string('email').notNullable().unique().alter();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.string('email').notNullable().alter();
+  });
+}
