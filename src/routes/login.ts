107d49f6c27486326a26be6a212e87b7132eb163
Alisson Vargas <contato@alissonsv.com>
2024-06-30 20:48:38 -0300

    chore: add swagger

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -4,37 +4,44 @@ import { JWT } from '../utils/jwt';
 import { UserRepository } from '../repositories/user-repository';
 import { PasswordHash } from '../utils/password-hash';
 import { validateLoginSchema } from './schemas/login-schema';
+import { loginSwaggerSchema } from './swagger-schema/login';
 
 export async function loginRoutes(app: FastifyInstance) {
-  app.post('/', async (request, reply) => {
-    let parsedLogin;
-    try {
-      parsedLogin = validateLoginSchema(request);
-    } catch (error) {
-      if (error instanceof Error) {
-        return reply.status(400).send({ error: error.message });
-      } else {
-        console.error(error);
-        return reply.status(500).send();
+  app.post(
+    '/',
+    {
+      schema: loginSwaggerSchema,
+    },
+    async (request, reply) => {
+      let parsedLogin;
+      try {
+        parsedLogin = validateLoginSchema(request);
+      } catch (error) {
+        if (error instanceof Error) {
+          return reply.status(400).send({ error: error.message });
+        } else {
+          console.error(error);
+          return reply.status(500).send();
+        }
       }
-    }
-    const { email, password } = parsedLogin;
+      const { email, password } = parsedLogin;
 
-    const userRepository = new UserRepository();
-    const user = await userRepository.readUserByEmail(email);
-    if (!user) {
-      return reply.status(401).send({ error: 'Email ou senha invalidos!' });
-    }
+      const userRepository = new UserRepository();
+      const user = await userRepository.readUserByEmail(email);
+      if (!user) {
+        return reply.status(401).send({ error: 'Email ou senha invalidos!' });
+      }
 
-    const isPasswordCorrect = await PasswordHash.verify(
-      user.password,
-      password,
-    );
-    if (!isPasswordCorrect) {
-      return reply.status(401).send({ error: 'Email ou senha invalidos!' });
-    }
+      const isPasswordCorrect = await PasswordHash.verify(
+        user.password,
+        password,
+      );
+      if (!isPasswordCorrect) {
+        return reply.status(401).send({ error: 'Email ou senha invalidos!' });
+      }
 
-    const token = JWT.createToken({ userId: user.id });
-    return { token };
-  });
+      const token = JWT.createToken({ userId: user.id });
+      return { token };
+    },
+  );
 }
