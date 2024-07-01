1742b7f4bc4c71660f5c1b39bcf501eb223c366f
Alisson Vargas <contato@alissonsv.com>
2024-07-01 19:27:06 -0300

    chore: add test coverage script

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
+    coverage: {
+      include: ['src/**/*.ts'],
+      exclude: ['src/server.ts', 'src/@types', 'src/env'],
+    },
     pool: 'forks',
     poolOptions: {
       forks: {
